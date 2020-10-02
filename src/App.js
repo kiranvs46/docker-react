b4113fe633e73e7d8c938d4165011f85c00235a8
kiran <kiranvs@qburst.com>
2020-10-02 22:47:54 +0530

    changed text in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello!
+          First full deployment!!!!!
         </p>
         <a
           className="App-link"
